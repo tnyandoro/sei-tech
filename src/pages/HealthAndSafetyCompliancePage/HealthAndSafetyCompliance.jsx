b13499ee59f271dfc879b75e0796026ff5ce4b34
Tendai <tnyandoro@gmail.com>
2025-02-22 20:53:50 +0200

    Fix missing pages

--- src/pages/HealthAndSafetyCompliancePage/HealthAndSafetyCompliance.jsx
+++ src/pages/HealthAndSafetyCompliancePage/HealthAndSafetyCompliance.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const HealthAndSafetyCompliance = () => {
   return (
     <div className="bg-gray-900 text-white min-h-screen flex flex-col p-6 pt-24 md:pt-28">
-      <Navbar />
 
       {/* Main Content */}
       <div className="flex-grow flex items-center justify-center p-6">
